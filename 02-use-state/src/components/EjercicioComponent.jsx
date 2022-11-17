d0203e5e14105d1a058fd175aadced97c3666297
JavatoX <cdpm98@hotmail.com>
2022-11-16 19:03:19 -0600

    update

---
+++ 02-use-state/src/components/EjercicioComponent.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+export const EjercicioComponent = ({ year }) => {
+  const [yearNow, setYearNow] = useState(year);
+
+  const siguiente = (e) => {
+    setYearNow(yearNow + 1);
+  };
+
+  const anterior = (e) => {
+    setYearNow(yearNow - 1);
+  };
+
+  const cambiarYear = (e) => {
+    let dato = parseInt(e.target.value);
+
+    if (Number.isInteger(dato)) {
+      setYearNow(dato);
+    } else {
+      setYearNow(year);
+    }
+  };
+
+  return (
+    <div>
+      <h2> Ejercicio con Eventos y UseState</h2>
+      <strong className="label label-green">{yearNow}</strong>
+      <p>
+        <button onClick={anterior}>Anterior</button>
+        &nbsp;
+        <button onClick={siguiente}>Siguiente</button>
+      </p>
+      <p>
+        Cambiar año:
+        <input onChange={cambiarYear} type="text" placeholder="Cambia el año" />
+      </p>
+    </div>
+  );
+};
+
+EjercicioComponent.propTypes = {
+  year: PropTypes.number.isRequired,
+};
