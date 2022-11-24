5d97daca9549cb03f8d814e11026a3191253f70b
Dani Plasc <cdpm98@hotmail.com>
2022-11-23 23:27:09 -0600

    seccion 14 - terminada

--- 14-mas-hooks/src/components/MiUsuario.jsx
+++ 14-mas-hooks/src/components/MiUsuario.jsx
@@ -1,36 +1,25 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
+import { useAjax } from "./useAjax";
 
 export const MiUsuario = () => {
-  const [usuario, setUsuario] = useState({
-    datos: null,
-    cargando: true,
-  });
-
-  const getUsuario = async (url) => {
-    const peticion = await fetch(url);
-    const { data: datos } = await peticion.json();
-
-    setUsuario({ datos, cargando: false });
-  };
-
+  const [url, setUrl] = useState("https://reqres.in/api/users/1");
+  const { datos, cargando } = useAjax(url);
   const getId = (e) => {
     let id = parseInt(e.target.value);
-    let url = `https://reqres.in/api/users/${id}`;
+    setUrl(`https://reqres.in/api/users/${id}`);
 
-    getUsuario(url);
+    // getUsuario(url);
   };
-
-  useEffect(() => {
-    getUsuario("https://reqres.in/api/users/1");
-  }, []);
 
   return (
     <div>
       <h1>Mi Usuario:</h1>
       <p>Datos del usuario:</p>
-      <p>{usuario.cargando ? "Cargando..." : ""}</p>
+      <p>{cargando ? "Cargando..." : ""}</p>
       {/* La "?" es como un if. Hace que no lo imprima si no existe. */}
-      <p>{usuario?.datos?.first_name}</p>
+      <p>
+        {datos?.first_name} {datos?.last_name}
+      </p>
       <input type="number" name="id" placeholder="" onChange={getId} />
     </div>
   );
