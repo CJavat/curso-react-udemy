5cd0bfb4498686eb96f9cedb7b2d886fb4d0f033
Dani Plasc <cdpm98@hotmail.com>
2022-11-24 02:05:46 -0600

    Proyecto: Mi_Blog - A la Mitad

---
+++ api-rest-node/index.js
@@ -0,0 +1,50 @@
+const { conexion } = require("./database/conexion");
+const express = require("express");
+const cors = require("cors");
+
+// Inicializar app.
+console.log("App de node arrancada");
+
+// Conectar a la DB.
+conexion();
+
+// Crear servidor Node.
+const app = express();
+const PORT = 3900;
+
+// Configurar CORs.
+app.use(cors());
+
+// Convertir body a objeto JS.
+app.use(express.json());
+
+// Crear rutas.
+app.get("/probando", (req, res) => {
+  console.log("Se ha ejecutado el endpoint PROBANDO");
+
+  return res.status(200).json([
+    {
+      curso: "Master en React",
+      autor: "CJavat",
+      url: "daniel.com",
+    },
+    {
+      curso: "Master en React",
+      autor: "CJavat",
+      url: "daniel.com",
+    },
+  ]);
+  // res.send("Hola mundo");
+});
+
+app.get("/", (req, res) => {
+  return res
+    .status(200)
+    .send("<h1> Empezando a crear una api rest con node </h1>");
+  // res.send("Hola mundo");
+});
+
+// Crear y escuchar peticiones http.
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto: ${PORT}`);
+});
