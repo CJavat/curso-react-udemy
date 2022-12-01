d6e81fd1a48ef1bde25f7e9870c4284ede2f54f9
JavatoX <cdpm98@hotmail.com>
2022-12-01 12:18:53 -0600

    Inicio del FrontEnd - Red Social

---
+++ 16-proyecto4/frontend-red-social/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="layout">
+      <h1>Proyecto red social</h1>;
+    </div>
+  );
+}
+
+export default App;
---
+++ 16-proyecto4/frontend-red-social/src/main.jsx
@@ -0,0 +1,12 @@
+// Importaciones obligatorias de React.
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// Import assets (Recursos: CSS, img, fuentes, etc.)
+import "./assets/fonts/fontawesome-free-6.1.2-web/css/all.css";
+import "./assets/css/normalize.css";
+import "./assets/css/styles.css";
+import "./assets/css/responsive.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
